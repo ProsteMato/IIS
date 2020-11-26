ad00dc01a571ad19cd5303f52574b11f24b487e8
Martin Koci <mkoci11@gmail.com>
2020-11-26 17:18:03 +0100

    created post entity and controller

---
+++ src/Entity/Post.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $rating;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creation_date;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Post::class, inversedBy="replays")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $post;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Post::class, mappedBy="post", orphanRemoval=true)
+     */
+    private $replays;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Thread::class, inversedBy="posts")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $thread;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="posts")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $created_by;
+
+    public function __construct()
+    {
+        $this->replays = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(int $rating): self
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creation_date;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creation_date): self
+    {
+        $this->creation_date = $creation_date;
+
+        return $this;
+    }
+
+    public function getPost(): ?self
+    {
+        return $this->post;
+    }
+
+    public function setPost(?self $post): self
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getReplays(): Collection
+    {
+        return $this->replays;
+    }
+
+    public function addReplay(self $replay): self
+    {
+        if (!$this->replays->contains($replay)) {
+            $this->replays[] = $replay;
+            $replay->setPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReplay(self $replay): self
+    {
+        if ($this->replays->removeElement($replay)) {
+            // set the owning side to null (unless already changed)
+            if ($replay->getPost() === $this) {
+                $replay->setPost(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getThread(): ?Thread
+    {
+        return $this->thread;
+    }
+
+    public function setThread(?Thread $thread): self
+    {
+        $this->thread = $thread;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?User
+    {
+        return $this->created_by;
+    }
+
+    public function setCreatedBy(?User $created_by): self
+    {
+        $this->created_by = $created_by;
+
+        return $this;
+    }
+}
