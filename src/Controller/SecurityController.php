589df58ab18443d82e03a3145fff1a1c064a31f3
MaggieMima <48593629+MaggieMima@users.noreply.github.com>
2020-11-27 11:04:52 +0100

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -34,7 +33,6 @@ class SecurityController extends AbstractController
 
         $user = new User();
 
-        $form = $this->createForm(LoginType::class, $user);
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -44,7 +42,6 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername,
                                                                 'loggedUser' => $loggedUser,
-                                                                'form' => $form->createView(),
                                                                 'error' => $error]);
     }
 
