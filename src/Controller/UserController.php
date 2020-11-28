ff72a9313866c1218ba524beddfc2f6cb72147a2
MaggieMima <48593629+MaggieMima@users.noreply.github.com>
2020-11-28 21:08:04 +0100

    Added recent posts to user profile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,6 +18,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use function Sodium\add;
 
 /**
  * Class UserController
@@ -46,13 +47,18 @@ class UserController extends AbstractController
         $user = $this->getDoctrine()->getRepository(User::class)->find($id);
 
         $groups = $loggedUser->getGroups();
+        $posts = $loggedUser->getPosts();
+        
 
         return $this->render('user/viewprofile.html.twig', [
             'showedUser' => $user,
             'loggedUser' => $loggedUser,
             'id' => $id,
             'groups' => $groups,
+            'posts' => $posts,
             'groups_count' => count($groups),
+            'threads_count' =>count($loggedUser->getThreads()),
+            'posts_count' => count($loggedUser->getPosts()),
             'controller_name' => 'UserController',
         ]);
     }
